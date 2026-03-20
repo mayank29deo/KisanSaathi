407d9da02d4533552d9cd120f4073bdcc919002e
priyaansuuu <priyanshurazzck56@gmail.com>
2026-03-20 21:55:18 +0530

    editing the state list in crop recommendation screen

--- src/screens/CropRecommendation.jsx
+++ src/screens/CropRecommendation.jsx
@@ -126,13 +126,33 @@ export default function CropRecommendation({ t }) {
             >
               <option value="">Select State</option>
               <option>Andhra Pradesh</option>
-              <option>Karnataka</option>
-              <option>Punjab</option>
-              <option>Maharashtra</option>
-              <option>Uttar Pradesh</option>
-              <option>Rajasthan</option>
+              <option>Arunachal Pradesh</option>
+              <option>Assam</option>
+              <option>Bihar</option>
+              <option>Chhattisgarh</option>
+              <option>Goa</option>
               <option>Gujarat</option>
+              <option>Haryana</option>
+              <option>Himachal Pradesh</option>
+              <option>Jharkhand</option>
+              <option>Karnataka</option>
+              <option>Kerala</option>
+              <option>Madhya Pradesh</option>
+              <option>Maharashtra</option>
+              <option>Manipur</option>
+              <option>Meghalaya</option>
+              <option>Mizoram</option>
+              <option>Nagaland</option>
+              <option>Odisha</option>
+              <option>Punjab</option>
+              <option>Rajasthan</option>
+              <option>Sikkim</option>
               <option>Tamil Nadu</option>
+              <option>Telangana</option>
+              <option>Tripura</option>
+              <option>Uttar Pradesh</option>
+              <option>Uttarakhand</option>
+              <option>West Bengal</option>
             </select>
           </div>
 
